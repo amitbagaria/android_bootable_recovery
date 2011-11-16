9e6ce2cc1c8b32e81add154dc5a584ce19e73fb5
Amit Bagaria <amit.bagaria@gmail.com>
2011-11-16 11:04:28 +0530

    Added CF-Root/Unroot option in CWM

--- Android.mk
+++ Android.mk
@@ -30,12 +30,7 @@ LOCAL_MODULE := recovery
 
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
-ifdef I_AM_KOUSH
-RECOVERY_NAME := ClockworkMod Recovery
-LOCAL_CFLAGS += -DI_AM_KOUSH
-else
-RECOVERY_NAME := CWM-based Recovery
-endif
+RECOVERY_NAME := CWM Recovery by amit.bagaria
 
 RECOVERY_VERSION := $(RECOVERY_NAME) v5.0.2.7
 
